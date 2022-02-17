73f26a7fbf499568bd0244ad138eb4ff083e0103
abradinjapan <abradinjapan@outlook.com>
2022-02-16 21:55:36 -0600

    Started page layout.

--- website-gen.py
+++ website-gen.py
@@ -1,5 +1,3 @@
-import string
-
 """
 
 Helper Classes
@@ -25,7 +23,7 @@ class ABOUT_style_data:
         output = ""
 
         for element in self.elements:
-            output += element.serialize()
+            output += element.serialize() + " "
         
         return output
 
@@ -92,29 +90,70 @@ def generate_page_head(page_title):
 
     return head
 
+def generate_page_contents_paragraph(msg):
+    output = ABOUT_tag("p")
+
+    output.attributes.append(ABOUT_attribute("style", ABOUT_css_element("text-align", "center").serialize()))
+    output.inner_tags.append(msg)
+
+    return output
+
 def generate_index():
     # colors
-    sea_green = "#00ffbf"
-    text_white = "#ffffff"
+    color_1 = "#00ffbf"
+    color_2 = "#eeeeee"
+    color_3 = "#000000"
+    color_4 = "#cccccc"
+    welcome_margin_length = "2%"
+    main_page_div_width = "60%"
+    font_1 = "Courier New"
     
     # style
-    main_style = ABOUT_style_data()
-    main_style.elements.append(ABOUT_css_element("background-color", sea_green))
-    white_text_style = ABOUT_style_data()
-    white_text_style.elements.append(ABOUT_css_element("color", text_white))
+    page_contents_div_style = ABOUT_style_data()
+    page_contents_div_style.elements.append(ABOUT_css_element("color", color_3))
+    page_contents_div_style.elements.append(ABOUT_css_element("background-color", color_2))
+    page_contents_div_style.elements.append(ABOUT_css_element("padding", welcome_margin_length))
+    header_div_style = ABOUT_style_data()
+    header_div_style.elements.append(ABOUT_css_element("margin-top", "0 auto"))
+    main_page_div_style = ABOUT_style_data()
+    main_page_div_style.elements.append(ABOUT_css_element("background-color", color_1))
+    main_page_div_style.elements.append(ABOUT_css_element("width", main_page_div_width))
+    main_page_div_style.elements.append(ABOUT_css_element("margin", "0 auto"))
+    main_page_div_style.elements.append(ABOUT_css_element("box-shadow", "0 10px 10px rgba(0, 0, 0, .5)"))
+    welcome_message_style = ABOUT_style_data()
+    welcome_message_style.elements.append(ABOUT_css_element("color", color_2))
+    welcome_message_style.elements.append(ABOUT_css_element("text-align", "center"))
+    welcome_message_style.elements.append(ABOUT_css_element("padding", welcome_margin_length))
+    body_style = ABOUT_style_data()
+    body_style.elements.append(ABOUT_css_element("background-color", color_2))
+    body_style.elements.append(ABOUT_css_element("font-family", font_1))
 
     # message
     msg = ABOUT_tag("h1")
-    msg.attributes.append(ABOUT_attribute("style", white_text_style))
-    msg.inner_tags.append("Hello!")
+    msg.attributes.append(ABOUT_attribute("style", welcome_message_style))
+    msg.inner_tags.append("Welcome!")
+
+    # page contents
+    page_contents_div = ABOUT_tag("div")
+    page_contents_div.attributes.append(ABOUT_attribute("style", page_contents_div_style))
+    page_contents_div.inner_tags.append(generate_page_contents_paragraph("This site is a work in progress."))
+    page_contents_div.inner_tags.append(generate_page_contents_paragraph("Working on it!"))
+    page_contents_div.inner_tags.append(generate_page_contents_paragraph(":)"))
+
+    # header
+    header_div = ABOUT_tag("div")
+    header_div.attributes.append(ABOUT_attribute("style", header_div_style))
+    header_div.inner_tags.append(msg)
 
     # main page div
     main_page_div = ABOUT_tag("div")
-    main_page_div.attributes.append(ABOUT_attribute("style", main_style))
-    main_page_div.inner_tags.append(msg)
+    main_page_div.attributes.append(ABOUT_attribute("style", main_page_div_style))
+    main_page_div.inner_tags.append(header_div)
+    main_page_div.inner_tags.append(page_contents_div)
 
     # page body
     body = ABOUT_tag("body")
+    body.attributes.append(ABOUT_attribute("style", body_style))
     body.inner_tags.append(main_page_div)
 
     # page html
